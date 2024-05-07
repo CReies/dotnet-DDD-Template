b8f148a11adbb8b8ad4d85000b9b103fa68cf594
Cristian Reyes <ccreyesmolina@gmail.com>
2024-05-07 05:16:13 -0500

    Add CreateAggregateUseCase to Application.Aggregate namespace

---
+++ src/DDD-Template/Application/Aggregate/CreateAggregateUseCase.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Generic;
+using Domain.Model.Aggregate.Commands;
+
+namespace Application.Aggregate;
+
+public class CreateAggregateUseCase( IEventsRepository repository ) : IInitialCommandUseCase<CreateAggregateCommand>
+{
+	private readonly IEventsRepository _repository = repository;
+
+	public List<DomainEvent> Execute( CreateAggregateCommand command )
+	{
+		var aggregate = new Domain.Model.Aggregate.Aggregate( command.Value );
+		var domainEvents = aggregate.GetUncommittedChanges();
+
+		domainEvents.ForEach( ( DomainEvent domainEvent ) => { _repository.Save( domainEvent )} );
+		aggregate.MarkAsCommitted();
+		return domainEvents;
+	}
+}
