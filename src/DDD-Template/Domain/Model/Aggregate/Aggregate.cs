b8bcc3916217d46088c76cdfa2ba7b1a3b5bf1f5
Cristian Reyes <ccreyesmolina@gmail.com>
2024-05-07 04:42:53 -0500

    Add Aggregate class to Domain.Model.Aggregate namespace

---
+++ src/DDD-Template/Domain/Model/Aggregate/Aggregate.cs
@@ -0,0 +1,36 @@
+﻿using Domain.Model.Aggregate.Events;
+using Domain.Model.Aggregate.Values.Example;
+using Domain.Model.Aggregate.Values.Root;
+
+namespace Domain.Model.Aggregate;
+
+public class Aggregate : AggregateRoot<AggregateId>
+{
+	public ExampleVO ExampleVO { get; set; }
+
+	public Aggregate( AggregateId identity ) : base( identity ) { }
+
+	public Aggregate( string exampleVO ) : base( new AggregateId() )
+	{
+		Subscribe( new AggregateBehavior( this ) );
+		ExampleVO = ExampleVO.Of( exampleVO );
+
+		AppendEvent( new AggregateCreated( Id.Value, ExampleVO.Value ) );
+	}
+
+	public static Aggregate From( string aggregateId, string exampleVO )
+	{
+		return new Aggregate( aggregateId )
+		{
+			ExampleVO = ExampleVO.Of( exampleVO )
+		};
+	}
+
+	public static Aggregate From( string aggregateId, List<DomainEvent> events )
+	{
+		var aggregate = new Aggregate( AggregateId.Of( aggregateId ) );
+		events.ForEach( aggregate.Apply );
+
+		return aggregate;
+	}
+}
