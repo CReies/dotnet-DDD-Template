6c92b4498f6d6e8e3735fd373f055c2aa4507b09
Cristian Reyes <ccreyesmolina@gmail.com>
2024-05-07 04:33:54 -0500

    Add AggregateId class to Domain.Model.Aggregate.Values.Root namespace

---
+++ src/DDD-Template/Domain/Model/Aggregate/Values/Root/AggregateId.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Model.Aggregate.Values.Root;
+
+public class AggregateId : Identity
+{
+	public AggregateId() : base() { }
+
+	private AggregateId( string value ) : base( value ) { }
+
+	public static AggregateId Of( string value ) => new AggregateId( value );
+}
