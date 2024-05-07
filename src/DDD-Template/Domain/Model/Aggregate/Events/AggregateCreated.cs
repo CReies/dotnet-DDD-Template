f8fb34271b5525fa62d5cdd6bed05b992975b5f7
Cristian Reyes <ccreyesmolina@gmail.com>
2024-05-07 04:42:36 -0500

    Rename ExampleCreated

--- src/DDD-Template/Domain/Model/Aggregate/Events/AggregateCreated.cs
+++ src/DDD-Template/Domain/Model/Aggregate/Events/AggregateCreated.cs
@@ -1,6 +1,6 @@
 ﻿namespace Domain.Model.Aggregate.Events;
 
-public class ExampleCreated( string aggregateId, string value ) : DomainEvent( EventsEnum.EXAMPLE_CREATED.ToString() )
+public class AggregateCreated( string aggregateId, string value ) : DomainEvent( EventsEnum.EXAMPLE_CREATED.ToString() )
 {
 	public new string AggregateId { get; set; } = aggregateId;
 	public string Value { get; set; } = value;
