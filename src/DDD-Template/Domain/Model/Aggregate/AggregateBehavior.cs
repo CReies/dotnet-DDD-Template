2d44143a93112434d8f43baa61c6fec6e88a47a6
Cristian Reyes <ccreyesmolina@gmail.com>
2024-05-07 04:34:00 -0500

    Add AggregateBehavior class to Domain.Model.Aggregate namespace

---
+++ src/DDD-Template/Domain/Model/Aggregate/AggregateBehavior.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Model.Aggregate.Events;
+using Domain.Model.Aggregate.Values.Example;
+
+namespace Domain.Model.Aggregate;
+
+public class AggregateBehavior : Behavior
+{
+	public AggregateBehavior( Aggregate aggregate )
+	{
+		AddExampleCreatedSub( aggregate );
+	}
+
+	private void AddExampleCreatedSub( Aggregate aggregate )
+	{
+		AddSub( ( ExampleCreated domainEvent ) =>
+		{
+			aggregate.ExampleVO = ExampleVO.Of( domainEvent.Value );
+		} );
+	}
+}
